f8f1f811a140c5c8eb64cdf554350a0f59f245d9
AbdSami081 <abdul.sami@global-supernova.com>
2026-01-29 19:12:41 +0500

    Removed the default Next.js icon to allow the branded logo to appear in the tab

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { LoginForm } from "./auth/login/page";
+import LoginPage from "./auth/login/page";
 
 export default function Home() {
   return (
@@ -13,14 +13,12 @@ export default function Home() {
     //      <Link href="/dashboard" className="underline text-blue-500 hover:text-blue-700 mb-4 block text-center">
     //         Get Started with SAP Integration
     //       </Link> */}
-         
+
     //     </main>    
     //   </div>
-    
-      <div className="flex min-h-svh w-full bg-gradient-to-b from-black to-neutral-900 items-center justify-center p-6 md:p-10">
-        <div className="w-full max-w-sm">
-          <LoginForm />
-        </div>
-      </div>
+
+    <LoginPage />
+
+
   );
 }
