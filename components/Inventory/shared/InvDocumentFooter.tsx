41ed816e217a4506af4ef61a5e8fea7f74d00538
AbdSami081 <abdul.sami@global-supernova.com>
2026-02-04 17:55:38 +0500

    Added totals and summary section for inventory module.

--- components/Inventory/shared/InvDocumentFooter.tsx
+++ components/Inventory/shared/InvDocumentFooter.tsx
@@ -14,7 +14,7 @@ import { useInventoryDocument } from "@/stores/inventory/useInventoryDocument";
 import { DocumentType } from "@/types/sales/salesDocuments.type";
 
 export default function InvDocumentFooter() {
-  const { watch } = useFormContext();
+  const { watch, register } = useFormContext();
   // const {
   //   comments
   // } = useInventoryDocument();
@@ -30,84 +30,28 @@ export default function InvDocumentFooter() {
     { Name: "Sara Malik", Remarks: "Assistant Sales Rep" },
   ];
 
-  const config = useInvDocConfig();
 
   return (
     <>
-      <div className={`flex items-center gap-4 mt-10`}>
-        {/* <Label className="text-sm w-28">Sales Employee :</Label>
-
-        <div className={`relative w-[250px] flex items-center `}>
-          <Select onValueChange={(v) => setSelectedEmployee(v)} value={selectedEmployee}>
-            <SelectTrigger className="w-full pr-10">
-              <SelectValue placeholder="Select employee" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {employees.map((emp) => (
-                  <SelectItem key={emp.Name} value={emp.Name}>
-                    {emp.Name}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-
-          <Settings
-            className="absolute right-3 h-5 w-5 cursor-pointer text-gray-500 hover:text-black"
-            onClick={() => setOpenModal(true)}
-          />
-        </div>
-
-        <GenericModal
-          open={openModal}
-          onClose={() => setOpenModal(false)}
-          onSelect={(name: any) => setSelectedEmployee(name)}
-          data={employees}
-          columns={[
-            { key: "Name", label: "Employee Name" },
-            { key: "Remarks", label: "Remarks" },
-          ]}
-          title="Select Sales Employee"
-          getSelectValue={(item) => item.Name}
-        /> */}
-      </div>
-
-      <div className="grid grid-cols-2 gap-20">
+      <div className="grid grid-cols-2 gap-10 -mt-0">
         <div>
-          <Label htmlFor="comments">Journal Remarks</Label>
+          <Label htmlFor="journalComments">Journal Remarks</Label>
           <Textarea
-            id="comments"
-            className="h-24 mt-4 max-w-95"
-            // value={comments}
-            // onChange={(e) => setComments(e.target.value)}
+            id="journalComments"
+            {...register("JournalMemo")}
+            className="h-24 mt-2 w-full"
             placeholder="Enter journal remarks..."
           />
         </div>
 
-        <div className={`space-y-3 p-4 rounded-lg text-sm -mt-18`}>
-            {config.type == DocumentType.InvTransferReq && (
-              <div>
-                <Label htmlFor="picknpack">Pick & pack remarks</Label>
-                <Textarea
-                    id="picknpack"
-                    className="h-24 mt-4 "
-                    // value={comments}
-                    // onChange={(e) => setComments(e.target.value)}
-                    placeholder="Enter remarks or comments..."
-                />
-            </div>
-            )} 
-            <div>
-                <Label htmlFor="comments">Remarks</Label>
-                <Textarea
-                    id="comments"
-                    className="h-24 mt-4 "
-                    // value={comments}
-                    // onChange={(e) => setComments(e.target.value)}
-                    placeholder="Enter remarks or comments..."
-                />
-            </div>
+        <div>
+          <Label htmlFor="remarks">Remarks</Label>
+          <Textarea
+            id="remarks"
+            {...register("Comments")}
+            className="h-24 mt-2 w-full"
+            placeholder="Enter remarks or comments..."
+          />
         </div>
       </div>
     </>
