4a4809db3ddbfa7a4bbad14e835e7c7453f4ef0b
AbdSami081 <abdul.sami@global-supernova.com>
2026-02-24 09:56:22 +0500

    integrated Guard for URL access control

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import HeaderNav from "@/components/header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { RouteGuard } from "@/components/route-guard";
 
 const DashboardLayout = ({
   children,
@@ -12,13 +13,15 @@ const DashboardLayout = ({
       <AppSidebar />
       <SidebarInset>
         <HeaderNav />
-            <div className="flex flex-1 flex-col">
-              <div className="@container/main flex flex-1 flex-col gap-2">
-                <div className="flex flex-col gap-4 py-2 md:gap-6 md:py-2 px-4 ">
-                    { children }
-                </div>
-              </div>
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-2 md:gap-6 md:py-2 px-4 ">
+              <RouteGuard>
+                {children}
+              </RouteGuard>
             </div>
+          </div>
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
