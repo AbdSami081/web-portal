84aa422be58375e550d9ab0c521660268f6679ff
Sami907 <61969622+Sami907@users.noreply.github.com>
2026-04-30 10:44:04 +0500

    Added API rewrite rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://192.168.19.16:7878/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
