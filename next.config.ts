3a81a2177c90e4203aaeb22595ed28056171aa39
Sami907 <61969622+Sami907@users.noreply.github.com>
2026-06-01 17:39:51 +0500

    set rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,24 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const REPORTING_API_URL = process.env.NEXT_PUBLIC_ReportingApi_URL;
-
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: false,
   output: "standalone",
 
   async rewrites() {
+    const apiBase = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "");
+    const reportingBase = process.env.NEXT_PUBLIC_ReportingApi_URL?.replace(/\/$/, "");
+
     return [
-      {
+      apiBase && {
         source: "/api/:path*",
-        destination: `${API_URL}/api/:path*`,
+        destination: `${apiBase}/api/:path*`,
       },
-      {
+
+      reportingBase && {
         source: "/reporting/:path*",
-        destination: `${REPORTING_API_URL}/:path*`,
+        destination: `${reportingBase}/:path*`,
       },
-    ];
+    ].filter(Boolean) as any;
   },
 };
 
